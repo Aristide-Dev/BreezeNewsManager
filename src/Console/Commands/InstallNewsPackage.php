1ec1a2f7c4784c98b97a35823ab5a5b3ffbd219a
AristechDev <aristidegnimassouu@gmail.com>
2025-02-12 12:07:15 +0000

    fixe bug

--- src/Console/Commands/InstallNewsPackage.php
+++ src/Console/Commands/InstallNewsPackage.php
@@ -3,6 +3,8 @@
 namespace AristechDev\NewsManager\Console\Commands;
 
 use Illuminate\Console\Command;
+use Symfony\Component\Process\Process;
+use Symfony\Component\Process\Exception\ProcessFailedException;
 
 class InstallNewsPackage extends Command
 {
@@ -30,9 +32,21 @@ class InstallNewsPackage extends Command
     {
         // Vérifier si Laravel Breeze est installé
         if (!class_exists(\Laravel\Breeze\BreezeServiceProvider::class)) {
-            $this->error("Laravel Breeze n'est pas installé. Veuillez l'installer via Composer :\n    composer require laravel/breeze");
-            $this->call('composer require laravel/breeze');
-            return;
+            $this->error("Laravel Breeze n'est pas installé. Installation automatique en cours...");
+
+            // utilisation de Symfony Process pour exécuter "composer require laravel/breeze"
+            $process = new Process(['composer', 'require', 'laravel/breeze']);
+            $process->setWorkingDirectory(base_path());
+            $process->run(function ($type, $buffer) {
+                $this->line($buffer);
+            });
+
+            if (!$process->isSuccessful()) {
+                $this->error("L'installation de Laravel Breeze a échoué. Veuillez l'installer manuellement : composer require laravel/breeze");
+                return;
+            }
+            
+            $this->info('Laravel Breeze a été installé avec succès.');
         }
 
         $this->info("Début de l'installation du package NewsManager.");
@@ -44,7 +58,6 @@ class InstallNewsPackage extends Command
             if ($stack) {
                 $this->info("Stack auto-détectée : $stack");
             } else {
-                // Si l'auto-détection échoue, proposer un choix interactif
                 $stack = $this->choice(
                     'Quelle stack frontale souhaitez-vous installer pour Laravel Breeze ?',
                     ['blade', 'react', 'vue'],
@@ -55,7 +68,7 @@ class InstallNewsPackage extends Command
             $this->info("Stack spécifiée par option : $stack");
         }
 
-        // Appel de la commande de Breeze pour installer le stack sélectionné
+        // Appel de la commande Breeze pour installer le stack sélectionné
         $this->call('breeze:install', ['stack' => $stack]);
 
         // 2. Choix des modules à installer (après l'installation du stack)
